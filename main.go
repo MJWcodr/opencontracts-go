d27a63340d19a3dd830d512359bc4e0a5657e1a9
mjwcodr <matthias@wuensch.xyz>
2025-10-10 16:11:35 +0200

    feature: add additional fields

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package opencontractinggo
+
+import (
+	"encoding/json"
+)
+
+type OpenContract struct {
+	URI string `json:"uri"`
+	Version string `json:"version"`
+	Extensions []string `json:"extensions"`
+	PublishedDate string `json:"publishedDate"`
+}
+
+// Load Function
+func (oc *OpenContract) Load(data []byte) error {
+	// Implementation to load data into the struct
+	return json.Unmarshal(data, oc)
+}
+
+// Print Function
+func (oc *OpenContract) Print() string {
+	return string(oc.URI + " - " + oc.Version + " - " + oc.PublishedDate)
+  :q
+}
+
+
