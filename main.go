51d61acd4534ef37fdf3020c310fd526113c0fa1
mjwcodr <matthias@wuensch.xyz>
2025-10-14 20:41:46 +0200

    feature: Add light extension support

--- main.go
+++ main.go
@@ -1,23 +1,13 @@
 package opencontractinggo
 
 import (
+	"bytes"
 	"encoding/json"
 	"os"
-	"bytes"
+	"slices"
 
 	"github.com/charmbracelet/log"
 )
-
-
-// New Function
-func New() *OpenContract {
-	return &OpenContract{}
-}
-
-// Print Function
-func (oc *OpenContract) Print() string {
-	return string(oc.URI + " - " + oc.Version + " - " + oc.PublishedDate)
-}
 
 type Address struct {
 	Street      string `json:"streetAddress"`
@@ -34,7 +24,6 @@ type ContactPoint struct {
 	Telephone string `json:"telephone"`
 	FaxNumber string `json:"faxNumber"`
 	Url       string `json:"url"`
-
 }
 
 type Identifier struct {
@@ -61,55 +50,56 @@ type Buyer struct {
 }
 
 type Tender struct {
-	ID                       string                `json:"id"`
-	Title                    string                `json:"title"`
-	Description              string                `json:"description"`
-	ProcuringEntity          TenderProcuringEntity `json:"procuringEntity"`
-	Items                    []TenderItem          `json:"items"`
-	Awards                   []TenderAward         `json:"awards"`
-	Language                 string                `json:"language"`
-	ProcurementMethodDetails string                `json:"procurementMethodDetails"`
-	MainProcurementCategory  string                `json:"mainProcurementCategory"`
-	NumberOfTenderers        int                   `json:"numberOfTenderers"`
-	Documents                []Document            `json:"documents"`
-	ReleaseOcid              string
-	Status                   string                `json:"status"`
-	MinValue                 MonetaryValue                `json:"minValue"`
-	Value                    MonetaryValue          `json:"value"`
-	ProcurementMethod        string                 `json:"procurementMethod"`
-	ProcurementMethodRationale string               `json:"procurementMethodRationale"`
-	AwardCriteria            string                 `json:"awardCriteria"`
-	AwardCriteriaDetails     string                 `json:"awardCriteriaDetails"`
-	SubmissionMethod         []string               `json:"submissionMethod"`
-	SubmissionMethodDetails         string               `json:"submissionMethodDetails"`
-	TenderPeriod Period `json:"tenderPeriod"`
-	EnquiryPeriod Period `json:"enquiryPeriod"`
-	AwardPeriod Period `json:"awardPeriod"`
-	ContractPeriod  Period   `json:"contractPeriod"`
-	HasEnquiries bool `json:"hasEnquiries"`
+	ID                         string                `json:"id"`
+	Title                      string                `json:"title"`
+	Description                string                `json:"description"`
+	ProcuringEntity            TenderProcuringEntity `json:"procuringEntity"`
+	Items                      []TenderItem          `json:"items"`
+	Awards                     []TenderAward         `json:"awards"`
+	Language                   string                `json:"language"`
+	ProcurementMethodDetails   string                `json:"procurementMethodDetails"`
+	MainProcurementCategory    string                `json:"mainProcurementCategory"`
+	NumberOfTenderers          int                   `json:"numberOfTenderers"`
+	Documents                  []Document            `json:"documents"`
+	ReleaseOcid                string
+	Status                     string        `json:"status"`
+	MinValue                   MonetaryValue `json:"minValue"`
+	Value                      MonetaryValue `json:"value"`
+	ProcurementMethod          string        `json:"procurementMethod"`
+	ProcurementMethodRationale string        `json:"procurementMethodRationale"`
+	AwardCriteria              string        `json:"awardCriteria"`
+	AwardCriteriaDetails       string        `json:"awardCriteriaDetails"`
+	SubmissionMethod           []string      `json:"submissionMethod"`
+	SubmissionMethodDetails    string        `json:"submissionMethodDetails"`
+	TenderPeriod               Period        `json:"tenderPeriod"`
+	EnquiryPeriod              Period        `json:"enquiryPeriod"`
+	AwardPeriod                Period        `json:"awardPeriod"`
+	ContractPeriod             Period        `json:"contractPeriod"`
+	HasEnquiries               bool          `json:"hasEnquiries"`
+	Lots                       []any         `json:"lots" extensions:lots` // Lots extension
 }
 
 type MonetaryValue struct {
-	Amount int `json:"amount"`
+	Amount   int    `json:"amount"`
 	Currency string `json:"currency"`
 }
 
 type Period struct {
-	StartDate string `json:"startDate"`
-	EndDate string `json:"endDate"`
-	DurationInDays int `json:"durationInDays"`
+	StartDate      string `json:"startDate"`
+	EndDate        string `json:"endDate"`
+	DurationInDays int    `json:"durationInDays"`
 }
 
 type Document struct {
-	ID          string   `json:"id"`
-	URL         string   `json:"url"`
-	RelatedLots []string `json:"relatedLots"`
-	DocumentType string `json:"documentType"`
-	Title       string  `json:"title"`
-	Description string `json:"description"`
-	DatePublished string `json:"datePublished"`
-	Format string `json:"format"`
-	Language string `json:"language"`
+	ID            string   `json:"id"`
+	URL           string   `json:"url"`
+	RelatedLots   []string `json:"relatedLots"`
+	DocumentType  string   `json:"documentType"`
+	Title         string   `json:"title"`
+	Description   string   `json:"description"`
+	DatePublished string   `json:"datePublished"`
+	Format        string   `json:"format"`
+	Language      string   `json:"language"`
 }
 
 type TenderProcuringEntity struct {
@@ -123,6 +113,10 @@ type TenderAward struct {
 	RelatedLots []string `json:"relatedLots"`
 }
 
+type DeliverLocation struct {
+	Description string `json:"description" extension:"deliveryLocation"`
+}
+
 type TenderItem struct {
 	ID                        string                   `json:"id"`
 	Classification            TenderItemClassification `json:"classification"`
@@ -130,23 +124,24 @@ type TenderItem struct {
 	Lots                      []TenderItemLot `json:"lots"`
 	RelatedLot                string          `json:"relatedLot"`
 	DeliveryAddress           Address         `json:"deliveryAddress"`
-	Description string `json:"description"`
-	Quantity int `json:"quantity"`
-	Unit TenderUnit `json:"unit"`
+	Description               string          `json:"description"`
+	Quantity                  int             `json:"quantity"`
+	Unit                      TenderUnit      `json:"unit"`
+	DeliveryLocation 					DeliverLocation `json:"deliveryLocation"`	
 }
 
 type TenderItemClassification struct {
 	Scheme      string `json:"scheme"`
 	ID          string `json:"id"`
 	Description string `json:"description"`
-	URI 				string `json:"uri"`
+	URI         string `json:"uri"`
 }
 
 type TenderUnit struct {
-	Name string `json:"name"` 
-	Id string `json:"id"` 
-	Scheme string `json:"scheme"`
-	Value MonetaryValue `json:"value"`
+	Name   string        `json:"name"`
+	Id     string        `json:"id"`
+	Scheme string        `json:"scheme"`
+	Value  MonetaryValue `json:"value"`
 }
 
 type TenderItemLot struct {
@@ -171,21 +166,21 @@ type Release struct {
 }
 
 type Publisher struct {
-	Name string `json:"name"`
+	Name   string `json:"name"`
 	Scheme string `json:"scheme"`
-	Uid string `json:"uid"`
-	Uri string `json:"uri"`
+	Uid    string `json:"uid"`
+	Uri    string `json:"uri"`
 }
 
 type OpenContract struct {
-	Version       string 		`json:"version"`
-	URI           string    `json:"uri"`
-	PublishedDate string    `json:"publishedDate"`
-	Extensions    []string  `json:"extensions"`
-	Releases      []Release `json:"releases"`
-	Publisher     Publisher  `json:"publisher"`
-	License       string  	`json:"license"`
-	PublicationPolicy string `json:"publicationPolicy"`
+	Version           string    `json:"version"`
+	URI               string    `json:"uri"`
+	PublishedDate     string    `json:"publishedDate"`
+	Extensions        []string  `json:"extensions"`
+	Releases          []Release `json:"releases"`
+	Publisher         Publisher `json:"publisher"`
+	License           string    `json:"license"`
+	PublicationPolicy string    `json:"publicationPolicy"`
 }
 
 func SynthesizeTenderID(release Release) Tender {
@@ -210,7 +205,7 @@ func SynthesizeTenderID(release Release) Tender {
 func SynthesizeTenderName(release Release) Tender {
 	tender := release.Tender
 
-	// check if the tender name is already set
+	// Check if the tender name is already set
 	if tender.Title != "" {
 		panic("This should only be called when the tender name is not set")
 	}
@@ -226,19 +221,45 @@ func SynthesizeTenderName(release Release) Tender {
 	return tender
 }
 
+func (oc *OpenContract) checkOCDSVersion() error {
+	// To be implemented
+	return nil
+}
+
+// checkExtensions checks for what extensions are used.
+// Based on the extensions additional fields may be availible in the OpenContract struct.
+// Since Go does not support dynamic fields, this function will just return an error if a field is defined
+// which is added by an extension that is not listed in the Extensions field.
+func (oc *OpenContract) checkExtensions() error {
+	supportedExtensions := map[string]string{
+		"Lots": "https://raw.githubusercontent.com/open-contracting-extensions/ocds_lots_extension/bd6a5a0617abcdcf9da19db0e70a899e6b10c91c/extension.json",
+	}
+
+	if !slices.Contains(oc.Extensions, supportedExtensions["Lots"]) {
+		// Check if the fields are not empty
+		// If they are not empty, return an error
+		// Since the Extension is not loaded.
+
+	}
+
+
+	return nil
+}
+
 func ParseJSONToBiddingOffer(byteArray []byte, strict bool) OpenContract {
 	var openContract OpenContract
 
+	
 	reader := bytes.NewBuffer(byteArray)
 	decoder := json.NewDecoder(reader)
 
 	if strict {
 		decoder.DisallowUnknownFields()
 	}
-	
+
 	err := decoder.Decode(&openContract)
 	if err != nil {
-		panic(err)	
+		panic(err)
 	}
 
 	// check for required fields
